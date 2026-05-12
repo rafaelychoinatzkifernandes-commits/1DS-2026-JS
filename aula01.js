e6c12c5205e36d4c428b1a55fff9142c6cfc13d5
RAFAELY CHOINATZKI <rafaely.choinatzki.fernandes@escola.pr.gov.br>
2026-05-12 10:02:16 -0300

    Add files via upload

---
+++ aula01.js
@@ -0,0 +1,15 @@
+alert("o carpa capim fica batendo no amiguinho!!!")
+
+console.log("Primeiro código em JS")
+console.log(4+5);
+ console.loh(3 * 4 );
+ console.log( true );
+ console.log( 3.89 );
+
+ console.log("Thiago" + " " +23+" " + 1.83+," "+ true);
+
+ 
+
+ "powermode.combo.couterEnabled",
+ "powermode.combo.counterSize": 0,
+ "powermode.shake.enabled" : false, 
